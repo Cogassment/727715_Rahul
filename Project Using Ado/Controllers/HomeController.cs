05e5fd35bea9b715928780822673bf3364729226
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-11 11:53:37 +0530

    Update HomeController.cs

--- Project Using Ado/Controllers/HomeController.cs
+++ Project Using Ado/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using RLG_Project_ADO_1.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -17,29 +18,30 @@ namespace RLG_Project_ADO_1.Controllers
         /// Summary
         public ActionResult Index()
         {
-            using (SqlConnection sqlConnection = new SqlConnection(constr))
-            {
-
-                SqlCommand sqlCommand = new SqlCommand("Select * From Items", sqlConnection);
-                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand.CommandText, sqlConnection);
-                sqlConnection.Open();
-                DataTable dataTable = new DataTable();
-                sqlDataAdapter.Fill(dataTable);
-                sqlConnection.Close();
-
-                List<ItemModel> itemModels = new List<ItemModel>();
-                foreach (DataRow item in dataTable.Rows.Cast<DataRow>().ToList())
+                using (SqlConnection sqlConnection = new SqlConnection(constr))
                 {
-                    ItemModel details = new ItemModel();
-                    details.Id = int.Parse(item[0].ToString());
-                    details.Name = (item[1].ToString());
-                    details.Cost = int.Parse(item[2].ToString());
-                    itemModels.Add(details);
-                }
 
-                return View(itemModels);
-            }
+                    SqlCommand sqlCommand = new SqlCommand("Select * From Items", sqlConnection);
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand.CommandText, sqlConnection);
+                    sqlConnection.Open();
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    sqlConnection.Close();
+
+                    List<ItemModel> itemModels = new List<ItemModel>();
+                    foreach (DataRow item in dataTable.Rows.Cast<DataRow>().ToList())
+                    {
+                        ItemModel details = new ItemModel();
+                        details.Id = int.Parse(item[0].ToString());
+                        details.Name = (item[1].ToString());
+                        details.Cost = int.Parse(item[2].ToString());
+                        itemModels.Add(details);
+                    }
+
+                    return View(itemModels);
+                }        
         }
+
         /// Summary
         /// // On Click Update Button, the following ActionResult will update the entered valid value in the database.
         /// Summary
@@ -47,17 +49,26 @@ namespace RLG_Project_ADO_1.Controllers
         public ActionResult UpdateItem(ItemModel item)
         {
             string sqlquery = "Update Items Set Name='" + item.Name + "',Cost=" + item.Cost + " where Id=" + item.Id;
-            using (SqlConnection sqlConnection = new SqlConnection(constr))
+            try
             {
-                sqlConnection.Open();
-                using (SqlCommand sqlCommand = new SqlCommand(sqlquery, sqlConnection))
+                using (SqlConnection sqlConnection = new SqlConnection(constr))
                 {
-                    sqlCommand.Connection = sqlConnection;
-                    sqlCommand.ExecuteNonQuery();
+                    sqlConnection.Open();
+                    using (SqlCommand sqlCommand = new SqlCommand(sqlquery, sqlConnection))
+                    {
+                        sqlCommand.Connection = sqlConnection;
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    sqlConnection.Close();
                 }
-                sqlConnection.Close();
+                return new EmptyResult();
             }
-            return new EmptyResult();
+            catch (Exception)
+            {
+
+                @TempData["UserMessage"] = "Update Data Failed";
+                return View();
+            }
         }
     }
 }
